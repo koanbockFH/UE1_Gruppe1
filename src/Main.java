52d684de5100d38d4f4d2016d9fb93e3609d52e4
Basset Axel Christian <1810653162@stud.fh-kufstein.ac.at>
2018-10-11 15:18:05 +0200

    Alter zur Ausgabe hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
+        int alter = 25;
         String vorname = "Axel";
-        System.out.println("Hello " + vorname + "!");
+        System.out.println(String.format("Hello %s! Du bist %d Jahre alt.", vorname, alter));
     }
 }
