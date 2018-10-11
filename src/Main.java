07afad7b22b8c45acfb5d6283f6360954a421efe
Basset Axel Christian <1810653162@stud.fh-kufstein.ac.at>
2018-10-11 15:41:14 +0200

    herkunft hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main
         String vorname = "Axel";
         String nachname = "Basset";
         int jahrgang = 2018;
-        System.out.println(String.format("Hello %s! Du bist %d Jahre alt. Dein Nachname ist: %s. Jahrgang = %d", vorname, alter, nachname, jahrgang));
+        String herkunft = "Buenos Aires";
+        System.out.println(String.format("Hello %s! Du bist %d Jahre alt. Dein Nachname ist: %s. Jahrgang = %d. Du kommst aus: %s", vorname, alter, nachname, jahrgang, herkunft));
     }
 }
